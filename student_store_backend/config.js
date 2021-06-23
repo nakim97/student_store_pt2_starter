36dc060dccfce36d6fd40139bc0255f166a3258c
Stella Kim <62909702+nakim97@users.noreply.github.com>
2021-06-23 11:34:49 -0700

    create JWT utils and set up auth middleware

--- student_store_backend/config.js
+++ student_store_backend/config.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 require("colors")
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3001
-
+const SECRET_KEY = process.env.SECRET_KEY || "secret_key"
 const IS_TESTING = process.env.NODE_ENV === "test"
 
 function getDatabaseUri() {
